f12f238ad91998dd2a23901019cb2920aaff6af7
BugHunter-X <askzenai@gmail.com>
2024-12-23 17:40:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,6 @@
+```javascript
+// Solution code using Tailwind CSS classes to fix the styling issue
+<div class="bg-gray-200 p-4">
+  <h1 class="text-3xl font-bold text-center">Hello, world!</h1>
+</div>
+```
